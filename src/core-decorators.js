81df9954db660d35979c7ee608bb8ce59c3f5312
Jay Phelps <hello@jayphelps.com>
2015-04-14 23:34:34 -0700

    Added @ memoize (naive first pass)

--- src/core-decorators.js
+++ src/core-decorators.js
@@ -1,3 +1,4 @@
 export { default as override } from './override';
 export { default as deprecated } from './deprecated';
 export { default as suppressWarnings } from './suppress-warnings';
+export { default as memoize } from './memoize';
---
+++ src/memoize.js
@@ -0,0 +1,68 @@
+import { decorate } from './private/utils';
+
+function toObject(cache, value) {
+  if (value === Object(value)) {
+    return value;  
+  }
+  return cache[value] || (cache[value] = {});
+}
+
+function applyAndCache(context, fn, args, cache, signature) {
+  const ret = fn.apply(context, args);
+  cache[signature] = ret;
+  return ret;
+}
+
+function metaForDescriptor(descriptor) {
+  let fn, wrapKey;
+
+  // This is ugly code, but way faster than other
+  // ways I tried that *looked* pretty
+  
+  if (descriptor.value) {
+    fn = descriptor.value;
+    wrapKey = 'value';
+  } else if (descriptor.get) {
+    fn = descriptor.get;
+    wrapKey = 'get';
+  } else if (descriptor.set) {
+    fn = descriptor.set;
+    wrapKey = 'set';
+  }
+
+  return { fn, wrapKey };
+}
+
+function handleDescriptor(target, key, descriptor) {
+  const { fn, wrapKey } = metaForDescriptor(descriptor);
+  const argumentCache = new WeakMap();
+  const signatureCache = Object.create(null);
+  const primativeRefCache = Object.create(null);
+  let argumentIdCounter = 0;
+  
+  return {
+    ...descriptor,
+    [wrapKey]: function memoizeWrapper() {
+      let signature = '0';
+      
+      for (const arg of arguments) {
+        let argRef = toObject(primativeRefCache, arg);
+        let argKey = argumentCache.get(argRef);
+        
+        if (argKey === undefined) {
+          argKey = ++argumentIdCounter;
+          argumentCache.set(argRef, argKey);
+        }
+        
+        signature += argKey;
+      }
+      
+      return signatureCache[signature]
+        || applyAndCache(this, fn, arguments, signatureCache, signature);
+    }
+  };
+}
+
+export default function memoize() {
+  return decorate(handleDescriptor, arguments);
+}
