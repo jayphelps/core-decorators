294ea3f6fe19917c9401e1e45e2439fb99d8382f
Frankie Bagnardi <f.bagnardi@gmail.com>
2015-08-25 20:00:27 -0700

    adds suppress-warnings test

---
+++ src/suppress-warnings.spec.js
@@ -0,0 +1,39 @@
+import chai from 'chai';
+import { spy } from 'sinon';
+import suppressWarnings from './suppress-warnings';
+
+const CONSOLE_WARN = console.warn;
+
+describe('suppressWarnings', function () {
+  class Foo {
+    @suppressWarnings
+    suppressed(){
+      console.warn('a');
+    }
+
+    unsuppressed(){
+      console.warn('b');
+    }
+  }
+
+  var warnSpy;
+  beforeEach(function () {
+    warnSpy = console.warn = spy();
+  });
+
+  afterEach(function () {
+    console.warn = CONSOLE_WARN;
+  });
+
+  it('suppresses warns', function () {
+    new Foo().suppressed();
+    warnSpy.called.should.equal(false);
+  });
+
+  it('restores the original console.warn', function () {
+    new Foo().suppressed();
+    new Foo().unsuppressed();
+    warnSpy.called.should.equal(true);
+    console.warn.should.equal(warnSpy);
+  });
+});
