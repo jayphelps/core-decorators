a7a194cf3f387a10b019caa1dff8ee3a04f4024b
Jay Phelps <hello@jayphelps.com>
2016-04-12 18:58:03 -0700

    Use 'decorate'  decorator on instances, not prototype, fixes #67

--- test/unit/decorate.spec.js
+++ test/unit/decorate.spec.js
@@ -1,19 +1,25 @@
 import decorate from '../../lib/decorate';
+import { memoize } from 'lodash';
 
 describe('@decorate', function () {
   let Foo;
 
   beforeEach(function () {
-    const decorator = function (fn, suffix) {
+    const append = function (fn, suffix) {
       return function (msg) {
         return fn(msg) + suffix;
       };
     };
 
     Foo = class Foo {
-      @decorate(decorator, '!!')
+      @decorate(append, '!!')
       suchWow(something) {
         return something + 'bro';
+      }
+
+      @decorate(memoize)
+      getFoo() {
+        return this;
       }
     }
   });
@@ -29,5 +35,17 @@ describe('@decorate', function () {
 
     desc.configurable.should.equal(true);
     desc.enumerable.should.equal(false);
+  });
+
+  it('is tied to the instance, not the prototype', function () {
+    const foo1 = new Foo();
+    const foo2 = new Foo();
+
+    const foo1Result = foo1.getFoo();
+    const foo2Result = foo2.getFoo();
+
+    foo1Result.should.not.equal(foo2Result);
+    foo1Result.should.equal(foo1);
+    foo2Result.should.equal(foo2);
   });
 });
