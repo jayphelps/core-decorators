11db70ada6205b48c1461ca5510563f98dc73d04
Burt Harris <Burt.Harris_github@azxs.33mail.com>
2017-06-01 02:17:10 -0700

    build-tsc build script cleanup
    With this change, both `npm run build` and `npm run build-ts` put their output in the __`lib`__ directory.  This allows us to compare behavior between the two versions of the library using `npm test` in either case.
    
    Right now, I'm getting 0 failures with the `build` (babel) version of the library, and 20 failures with the `build-tsc` (TypeScript) version.
    
    - clean out the built directory before tsc
    - remove hack to ignore erros from tsc
    - checkin vscode launch/debug files
    - compile _only_ the `src` directory with tsc
    - switch back to default import of sinonChai in test.spec.js

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,6 +1,6 @@
 import * as chai from 'chai';
-import * as sinon from 'sinon';
-import * as sinonChai from 'sinon-chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
 
 chai.should();
 chai.use(sinonChai);
