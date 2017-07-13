e69968a74d7ecc95c64783eb45b0ee9602f52e52
duncant <djmtweed@gmail.com>
2017-07-13 17:22:19 +1200

    Removed deprecated typings, removed typings that already exist in the core libraries and added in new EOF newline

--- index.d.ts
+++ index.d.ts
@@ -1,48 +1,26 @@
-// Type definitions for core-decorators.js 0.10
+// Type definitions for core-decorators.js 0.19
 // Project: https://github.com/jayphelps/core-decorators.js
 // Definitions by: Qubo <https://github.com/tkqubo>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1
-
-export interface ClassDecorator {
-    <TFunction extends Function>(target: TFunction): TFunction | void;
-}
-
-export interface ParameterDecorator {
-    (target: object, propertyKey: string | symbol, parameterIndex: number): void;
-}
-
-export interface PropertyDecorator {
-    (target: object, propertyKey: string | symbol): void;
-}
-
-export interface MethodDecorator {
-    <T>(target: object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>): TypedPropertyDescriptor<T> | void;
-}
+//                 dtweedle <https://github.com/dtweedle>
+// TypeScript Version: 2.4.1
 
 export interface PropertyOrMethodDecorator extends MethodDecorator, PropertyDecorator {
-    (target: object, propertyKey: string | symbol): void;
+  (target: object, propertyKey: string | symbol): void;
 }
 
 export interface Deprecate extends MethodDecorator {
-    (message?: string, option?: DeprecateOption): MethodDecorator;
+  (message?: string, option?: DeprecateOption): MethodDecorator;
 }
 
 export interface DeprecateOption {
-    url: string;
+  url: string;
 }
 
 export interface ThrottleOptions {
-    /** allows to trigger function on the leading. */
-    leading?: boolean;
-    /** allows to trigger function on the trailing edge of the wait interval. */
-    trailing?: boolean;
-}
-
-export interface Console {
-    log(message?: any, ...optionalParams: any[]): void;
-    time(timerName?: string): void;
-    timeEnd(timerName?: string): void;
+  /** allows to trigger function on the leading. */
+  leading?: boolean;
+  /** allows to trigger function on the trailing edge of the wait interval. */
+  trailing?: boolean;
 }
 
 /**
@@ -66,14 +44,6 @@ export const deprecate: Deprecate;
  * Calls console.warn() with a deprecation message. Provide a custom message to override the default one. You can also provide an options hash with a url, for further reading.
  */
 export const deprecated: Deprecate;
-/**
- * Creates a new debounced function which will be invoked after wait milliseconds since the time it was invoked. Default timeout is 300 ms.
- */
-export function debounce(wait: number): MethodDecorator;
-/**
- * Creates a new throttled function which will be invoked in every wait milliseconds. Default timeout is 300 ms.
- */
-export function throttle(wait: number, options?: ThrottleOptions): MethodDecorator;
 /**
  * Suppresses any JavaScript console.warn() call while the decorated function is called. (i.e. on the stack)
  */
@@ -100,16 +70,6 @@ export function decorate(func: Function, ...args: any[]): MethodDecorator;
  * Useful to prevent excess allocations that might otherwise not be used, but be careful not to over-optimize things.
  */
 export const lazyInitialize: PropertyDecorator;
-/**
- * Mixes in all property descriptors from the provided Plain Old JavaScript Objects (aka POJOs) as arguments.
- * Mixins are applied in the order they are passed, but do not override descriptors already on the class, including those inherited traditionally.
- */
-export function mixin(...mixins: any[]): ClassDecorator;
-/**
- * Mixes in all property descriptors from the provided Plain Old JavaScript Objects (aka POJOs) as arguments.
- * Mixins are applied in the order they are passed, but do not override descriptors already on the class, including those inherited traditionally.
- */
-export function mixins(...mixins: any[]): ClassDecorator;
 /**
  * Uses console.time and console.timeEnd to provide function timings with a unique label whose default prefix is ClassName.method. Supply a first argument to override the prefix:
  */
