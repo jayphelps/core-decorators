175205240212e36f31c507e25b24ac4a1d3b95a1
Burt Harris <Burt.Harris_github@azxs.33mail.com>
2017-07-27 16:49:15 -0700

    Build tests w/ Babel at build time
    They build into directory testBabel.  With this change, we no longer need to use mocha switchs `--compilers js:babel-core/register` or `--require babel-polyfill`

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-var gulp = require('gulp');
-var ts = require('gulp-typescript');
-var sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const ts = require('gulp-typescript');
+const sourcemaps = require('gulp-sourcemaps');
 const rimraf = require('rimraf');
 
 // Default build uses settings from 'tsconfig.json'
@@ -31,11 +32,36 @@ gulp.task('esm', function () {
       .pipe(gulp.dest('esm'))
 });
 
-
 gulp.task('src', ['cjs', 'esm']);
 
-gulp.task('watch', ['src'], function () {
+gulp.task('babel.test', ['src'], function () {
+  return gulp.src('test/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      "presets": [["es2015", { "modules": false }]],
+      "plugins": [
+        "transform-object-rest-spread",
+        "transform-flow-strip-types",
+        "transform-decorators-legacy",
+        "transform-class-properties"
+      ],
+    "env": {
+      "es": {},
+      "development": {
+        "plugins": [
+          "transform-es2015-modules-commonjs"
+        ]
+      }}}))
+    .pipe(sourcemaps.write('sourcemaps'))
+    .pipe(gulp.dest('testBabel'))
+})
+
+
+gulp.task('test', ['babel.test'] )
+
+gulp.task('watch', ['src','test'], function () {
   gulp.watch('src/**/*', ['src'])
+  gulp.watch('test/**/*', ['test'])
 });
 
-gulp.task('default', ['src']);
+gulp.task('default', ['src', 'test']);
